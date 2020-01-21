e86e06e5f464e5f15379899aa3002546eabb0f55
KaspardMurat <gaspardmurat4@gmail.com>
2020-01-21 17:20:54 +0900

    adding parse_option.sh in local folder

--- path.sh
+++ path.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 MAIN_ROOT=$PWD
+
+if [ -e $MAIN_ROOT/tools/venv/etc/profile.d/conda.sh ]; then
+    source $MAIN_ROOT/tools/venv/etc/profile.d/conda.sh && conda deactivate && conda activate
+else
+    source $MAIN_ROOT/tools/venv/bin/activate
+fi
+
 export PATH=${MAIN_ROOT}:${MAIN_ROOT}/bin:$PATH
--- run.sh
+++ run.sh
@@ -2,42 +2,45 @@
 #!/usr/bin/env python3
 #!/usr/bin/env python2
 
+echo "running path.sh"
 . ./path.sh
 
 # general configuration
+
 stage=0
 dataset_master_folder=./dataset_master
 exp=./exp
 
 type='train'
 
+. local/parse_options.sh || exit 1;
+
 fps=25
 sampling=44100
 hop_length=1764
 wlen=256
 
+echo "============================================================"
+echo "                        Music2Dance"
+echo "============================================================"
+
 if [ ${stage} -eq 0 ]; then
   echo "stage 0: preparing motion file..."
-  motion_prepare.py -fps ${fps} \
-                    -sampling ${sampling} \
-                    -hop_length ${hop_length} \
-                    -wlen ${wlen} \
-                    -snr 0 \
-                    -folder ${dataset_master_folder} \
-                    -save ${exp} \
-                    -type ${train} || exit 1;
+  motion_prepare.py -f ${fps} \
+                    -sr ${sampling} \
+                    -hl ${hop_length} \
+                    -w ${wlen} \
+                    -r 0 \
+                    -d ${dataset_master_folder} \
+                    -o ${exp} \
+                    -t ${type} || exit 1;
 fi
 echo "----- End-to-End stage"
 
 if [ ${stage} -eq 1 ]; then
-  echo "stage 0: preparing motion file..."
-  motion_audio_treatment.py -fps ${fps} \
-                            -sampling ${sampling} \
-                            -hop_length ${hop_length} \
-                            -wlen ${wlen} \
-                            -snr 0 \
-                            -folder ${dataset_master_folder} \
-                            -save ${exp} \
-                            -type ${train} || exit 1;
+  echo "stage 1: whatever..."
+
 fi
 echo "----- End-to-End stage"
+
+echo "`basename $0` Done."
