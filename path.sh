0a7f524973bae127fb509b36e61e1ef4e4744f83
KaspardMurat <gaspardmurat4@gmail.com>
2020-01-22 13:35:41 +0900

    changing path.sh (changing source command

--- path.sh
+++ path.sh
@@ -3,9 +3,9 @@
 MAIN_ROOT=$PWD
 
 if [ -e $MAIN_ROOT/tools/venv/etc/profile.d/conda.sh ]; then
-    source $MAIN_ROOT/tools/venv/etc/profile.d/conda.sh && conda deactivate && conda activate
+    . $MAIN_ROOT/tools/venv/etc/profile.d/conda.sh && conda deactivate && conda activate
 else
-    source $MAIN_ROOT/tools/venv/bin/activate
+    . $MAIN_ROOT/tools/venv/bin/activate
 fi
 
 export PATH=${MAIN_ROOT}:${MAIN_ROOT}/bin:$PATH
